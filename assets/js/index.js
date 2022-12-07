e0e3aabade113b7b4c59689eca578b883f99370b
marcus-borger <57198057+marcus-borger@users.noreply.github.com>
2022-12-06 21:47:14 -0500

    update portfolio
    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,25 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const carousels = document.querySelectorAll(".carousel");
-    const sidenav = document.querySelectorAll(".sidenav");
-    const parallax = document.querySelectorAll(".parallax");
-  
-    M.Sidenav.init(sidenav);
-    M.Parallax.init(parallax);
-    
-    M.Carousel.init(carousels, {
-        numVisible: 5,
-        padding: 125
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const carousels = document.querySelectorAll(".carousel");
+  const sidenav = document.querySelectorAll(".sidenav");
+  const parallax = document.querySelectorAll(".parallax");
 
+  M.Sidenav.init(sidenav);
+  M.Parallax.init(parallax);
 
+  M.Carousel.init(carousels, {
+    numVisible: 5,
+    padding: 125,
+  });
 });
+/**
+ * Smooth scroll to anchor point
+ */
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
